6f54f742b6954ac674f9adf68e05a91b076ae5e9
Fernando Beltran <fernando.beltran.brandez@gmail.com>
2020-04-15 18:36:46 +0200

    Leyendo el txt de forma asíncrona

--- src/index.js
+++ src/index.js
@@ -1,27 +1,30 @@
-//console.log('Hola Mundo')
+// Muestra el número total de lineas, y el número de palabras por linea
 
-//importamos las librerias para leer texto
+//Importamos la libreria necesario
 import fs from 'fs'
-//import * as fs from 'fs'
-import readline from 'readline'
-
-//Cogemos el segundo argumento recibido en la ejecución, que será el número del fichero
+//Cogemos el segundo parámetro escrito en la instrucción en terminal
 const file = process.argv[2]
-let lines = 0
-//console.log(file)
 
-//Creamos la constante para leer
-const rl = readline.createInterface({
-    input: fs.createReadStream(file),
-    crlfDelay: Infinity
+//Leemos el fichero y cuando acabe ejecutamos la función recibiendo los datos escritos entre paréntesis
+//file es el fichero leido, err es un posible error y contents es lo que ha leido
+fs.readFile(file, (err, contents) => {
+  //Si da error, que nos informe por pantalla del problema y paramos la ejecución
+  if (err) {
+    return console.log(err)
+  }
+
+  //Convertimos contents en un string con toString y lo dividimos en parámetros del array lines con split
+  //guiandonos por los saltos de linea
+  const lines = contents.toString().split('\n')
+
+  //leemos cada una de las lineas
+  for (let line of lines) {
+    //El length de line indicará el tamaño de la linea en la que estamos en carácteres
+    console.log(`Número de caracteres por linea: ${line.length}`)
+  }
+
+  //El length de lines nos indicará el número de parámetros en el array
+  console.log(`\nNúmero total de lineas: ${lines.length}`)
 })
 
-//Vamos contando las lineas y mostrando el número de carácteres en cada linea
-rl.on('line', line =>{
-    ++lines
-    console.log(`Numero total de caracteres por linea: ${line.length}`)
-})
-
-//Mostramos la suma de todas las lineas
-rl.on('close', () => console.log(`Numero total de lineas: ${lines}`))
-
+console.log(`Fichero selecionado: ${file}\n`)
