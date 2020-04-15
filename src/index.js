5bc78b213ad356b8fa3be9df655777cda2ece16f
Fernando Beltran <fernando.beltran.brandez@gmail.com>
2020-04-15 15:34:47 +0200

    Modificado index.js para leer un fichero de texto

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 //console.log('Hola Mundo')
-const value = 24
 
-const miFn = num => num * 10
+//importamos las librerias para leer texto
+import fs from 'fs'
+//import * as fs from 'fs'
+import readline from 'readline'
 
-console.log(miFn(value))
+//Cogemos el segundo argumento recibido en la ejecución, que será el número del fichero
+const file = process.argv[2]
+let lines = 0
+//console.log(file)
+
+//Creamos la constante para leer
+const rl = readline.createInterface({
+    input: fs.createReadStream(file),
+    crlfDelay: Infinity
+})
+
+//Vamos contando las lineas y mostrando el número de carácteres en cada linea
+rl.on('line', line =>{
+    ++lines
+    console.log(`Numero total de caracteres por linea: ${line.length}`)
+})
+
+//Mostramos la suma de todas las lineas
+rl.on('close', () => console.log(`Numero total de lineas: ${lines}`))
+
