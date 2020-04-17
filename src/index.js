354c73e6e9f3b42e6168262cf6f535e5f771a12a
Fernando Beltran <fernando.beltran.brandez@gmail.com>
2020-04-17 18:08:05 +0200

    Instalando y configurando nodemon para la ejecución en tiempo real del código fuente escrito

--- src/index.js
+++ src/index.js
@@ -1,18 +1,1 @@
-//Probamos dos tipos de error. Aquí lo que hacemos es capturar los errores con esos dos eventos
-
-process.on('unhandledRejection', (err, p) => {
-  console.log('Custom Error: An unhandledRejection occurred')
-  console.log(`Custom Error: Rejection: ${err}`)
-})
-
-process.on('uncaughtException', err => {
-  console.log('Custom Error: An uncaughtException occurred')
-  console.log(`Custom Error: Rejection: ${err}`)
-})
-
-setTimeout(() => console.log('This will still run.'), 500)
-
-// El error es JSON.pasre
-Promise(resolve => JSON.pasre({ color: 'azul' }))
-// test()
-// throw 'casa'
+console.log('Hola que tal')
