e90268df188e5155a4161603da35788af842b6d8
Fernando Beltran <fernando.beltran.brandez@gmail.com>
2020-04-28 15:20:16 +0200

    Añadidas variables de entorno

--- src/config.js
+++ src/config.js
@@ -4,6 +4,12 @@ import bodyParser from 'body-parser'
 import logger from 'morgan'
 //Importamos cors
 import cors from 'cors'
+//Importamos solo la funcion config de la dependencia dotenv
+import { config } from 'dotenv'
+
+//Cargamos la configuracion en la constante settings. Esto guarda toda la informacion del
+//fichero de configuracion
+const SETTINGS = config()
 
 //Exportamos (la vamos a utilizar fuera) una funcion que por defecto recibirá la instancia del
 //servidor creado
@@ -13,7 +19,16 @@ export default app => {
  
   //Seteamos el entorno de desarrollo. Le decimos que nuestro entorno se define por
   //process.env.NODE_ENV
-  app.set('env', process.env.NODE_ENV)
+  //app.set('env', process.env.NODE_ENV)
+  //El entorno de desarrollo vendrá definido en la configuracion que esta almacenada en SETTINGS
+  app.set('env', SETTINGS.parsed.ENV)
+ 
+  //Seteamos la variable config y le decimos que guarde todos los datos de settings
+  app.set('config', SETTINGS.parsed)
+  //Le decimos que de forma local el entorno es env (variable seteada arriba)
+  app.locals.env = app.get('env')
+  //Seteamos el config en el config local
+  app.locals.config = app.get('config')
 
   //Desactivamos el logger en caso de los test
   if (process.env.NODE_ENV !== 'test') {
