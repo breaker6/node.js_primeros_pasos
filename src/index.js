d5a4c1f78a5fe93a6e04e204dc51b8b69c57c4af
Fernando Beltran <fernando.beltran.brandez@gmail.com>
2020-04-28 15:14:54 +0200

    Iniciado el servidor

--- src/index.js
+++ src/index.js
@@ -1,58 +1,34 @@
-//Importamos la libreria de express
+//Importamos express
 import express from 'express'
-//Importamos body-parser.Es un middleware que parsea de forma automatica la respuesta del servidor
-//metiendola en un campo body dentro del response
-import bodyParser from 'body-parser'
-//Importamos la libreria morgan. Morgan esta pensado como middleware
-import logger from 'morgan'
-//Importamos la libreria requerida para rutas
-import path from 'path'
-//Importamos la libreria que gestiona el enrutado
-import router from './router'
 
-//Cargamos una instancia de express dentro de una variable
-const app = express()
+//Guardamos la instancia
+let _server
 
-//Desactivar el aviso de que nuestra web esta hecho con express en la cabecera del servidor
-app.disable('x-powered-by')
+//Generamos el objeto server con los metodos start y close
+const server = {
+  start () {
+    //Creamos el servidor en express
+    const app = express()
 
-//Le definimos que en el entorno en el que estamos es development
-app.set('env', 'development')
+    //Guardamos la instancia y le decimos que escuche el puerto 9000
+    _server = app.listen('9000', () => {
+      //Si el entorno en el que estamos ejecutando es distinto de test
+      if (process.env.NODE_ENV !== 'test') {
+        //Que nos avise cuando este arrancando
+        console.log('Server opened listen on http://localhost:9000')
+      }
+    })
+  },
+  //En close cerramos la instancia del servidor
+  close () {
+    _server.close()
+  }
+}
 
-//Genera un log que nos dará una serie de datos en el servidor. Combined es el tipo de log
-app.use(logger('combined'))
-//Le decimos que use body-parser y que devuelva un json
-app.use(bodyParser.json())
-//Le decimos que el codificado no sea extendido
-app.use(bodyParser.urlencoded({ extended: false }))
+//Exportamos el objeto server
+export default server
 
-//Le decimos en que ruta están las vistas a utilizar. Este será el resultado de concatenar
-//el directorio actual en el que estamos mas views
-app.set('views', path.join(__dirname, 'views'))
-//Le definimos que el motor de plantillas a utilizar (view engine). En este caso es pug
-app.set('view engine', 'pug')
-
-//RUTAS
-router(app)
-
-//Le decimos que cuando detecte una petición a /static tiene que devolver un archivo del
-//directorio public. Gracias a esto podremos cargar el css.
-app.use(
-  '/static',
-  express.static(path.join(__dirname, 'public'))
-)
-
-//Ejecutaremos esta ruta si se accede a una ruta no definida
-app.use((req, res, next) => {
-  res.render('404', {
-    title: 'NodeJS',
-    message: `La ruta no existe!!!!!`
-  })
-  next()
-})
-
-//Le decimos a express que escuche el puerto 9000
-app.listen('9000', () => {
-  //Cuando el servidor arranca nos mostrará el siguiente mensaje
-  console.log('Server opened listen on http://localhost:9000')
-})
+//Si se utiliza como modulo principal, que ejecute server.start()
+if (!module.parent) {
+  server.start()
+}
